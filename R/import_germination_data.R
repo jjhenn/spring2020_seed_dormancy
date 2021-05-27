3eea780a12c5b3196a7cf56f3ae6d27c204c2a4d
Jonathan Henn <jonathan.henn@bioenv.gu.se>
2021-05-27 14:31:43 -0500

    start compiling data

---
+++ R/import_germination_data.R
@@ -0,0 +1,53 @@
+library(readxl)
+library(tidyverse)
+
+data1 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 2, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data2 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 3, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date)) %>% 
+  rename("NOTES" = "entry_checked_date")
+
+data3 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 4, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data4 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 5, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data5 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 6, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data6 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 7, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data7 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 8, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+data8 <- read_excel("G:/My Drive/Grad School/Projects/active/spring2020_seed_dormancy/data/germination_data.xlsx", sheet = 9, col_types = "text") %>% 
+  pivot_longer(cols = c(6:(ncol(.)-1)), names_to = "date", values_to = "n_germ") %>% 
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         start_date = as.Date(as.numeric(start_date), origin = "1899-12-30")) %>% 
+  filter(!is.na(date))
+
+comp_data <- bind_rows(data1, data2, data3, data4, data5, data6, data7, data8)
